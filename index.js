84f3ca096f32c3d735ba8869888d1f4a90f47c3b
AnshMittal1811 <anshm18111996@gmail.com>
2023-07-21 01:23:14 -0700

    Made changes for MongoDB promises

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag');
 const mongoose  = require('mongoose');
 
+const { MONGODB } = require('/config.js')
+
+
+const Post = require('./models/Post.js');
+
+// Tag Template strings
 const typeDefs = gql`
     type Query{
-        sayHi: String!
+        getPosts:
     }
 `
 
+//  For each query or mutation, each has its own resolvers
+// Group all the queries inside the query obj and all
+// mutations inside the mutations obj
 const resolvers = {
     Query: {
         sayHi: () => 'Hello world!'
@@ -19,6 +28,15 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000 }).then((response) => {
+// Server.listen returns (port: port no)
+// returns a promise; whats a promise?
+
+// Apollo server runs on Express 
+// Don't write any specific express routes
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+    .then(() => {
+        console.log("MongoDB connected");
+        return server.listen({port: 5000});
+}).then((response) => {
     console.log(`Server running at ${response.url}`);
 });
